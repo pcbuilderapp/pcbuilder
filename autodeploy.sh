f71e8dc6dd7c28c5cfbf598b24141e73c57be8a4
root <root@WFGN001.cs1local>
2017-01-15 20:27:34 +0000

    script fix

--- autodeploy.sh
+++ autodeploy.sh
@@ -3,5 +3,6 @@ cd <path>
 git pull
 mvn deploy
 docker stop pcbuilder-backend
+docker rm pcbuilder-backend
 docker build -t pcbuilder/backend .
 docker run --name "pcbuilder-backend" -d -p 8090:8090 -t pcbuilder/backend --link pcbuilder-mysql:mysql
