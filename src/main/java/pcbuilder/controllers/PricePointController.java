2128bcec0efd2c07de38c67002c9b4ff4b11afab
Stefan Ladage <sladage@gmail.com>
2017-01-25 21:02:03 +0100

    Added access origin header annotation.

--- src/main/java/pcbuilder/controllers/PricePointController.java
+++ src/main/java/pcbuilder/controllers/PricePointController.java
@@ -1,6 +1,7 @@
 package pcbuilder.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -22,6 +23,7 @@ public class PricePointController {
      *
      * @return all the price points
      */
+    @CrossOrigin(origins = "*")
     @RequestMapping(value="/pricepoint/getall", method= RequestMethod.GET)
     public Iterable<PricePoint> getAllPricePoints() {
         return pricePointRepository.findAll();
