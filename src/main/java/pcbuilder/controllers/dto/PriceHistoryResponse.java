b3168d59d5059698c67efecac8934aba2951b38d
Daniel de Witte <danieldewitte@gmail.com>
2017-01-28 23:17:16 +0100

    added priceFalling to PriceHistoryResponse

--- src/main/java/pcbuilder/controllers/dto/PriceHistoryResponse.java
+++ src/main/java/pcbuilder/controllers/dto/PriceHistoryResponse.java
@@ -1,5 +1,7 @@
 package pcbuilder.controllers.dto;
 
+import pcbuilder.domain.PricePoint;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -9,6 +11,8 @@ import java.util.List;
 public class PriceHistoryResponse {
 
     private List<PricePointData> priceHistory = new ArrayList<>();
+
+    private boolean priceFalling;
 
     /**
      * Gets the list with PriceHistory data
@@ -24,6 +28,7 @@ public class PriceHistoryResponse {
      */
     public void setPriceHistory(List<PricePointData> priceHistory) {
         this.priceHistory = priceHistory;
+        calculatePriceFalling();
     }
 
     /**
@@ -31,6 +36,34 @@ public class PriceHistoryResponse {
      * @param pricePointData PricePointData
      */
     public void addPriceHistory(PricePointData pricePointData) {
+
         priceHistory.add(pricePointData);
+        calculatePriceFalling();
+    }
+
+    /**
+     * Has the price of this history gone up or down?
+     * @return boolean
+     */
+    public boolean isPriceFalling() {
+        return priceFalling;
+    }
+
+    /**
+     * Sets if the price of this history has gone up or down
+     * @param priceFalling boolean
+     */
+    private void setPriceFalling(boolean priceFalling) {
+        this.priceFalling = priceFalling;
+    }
+
+    private void calculatePriceFalling() {
+        PricePointData first = priceHistory.get(0);
+        PricePointData last = priceHistory.get(priceHistory.size());
+        if (first.getPrice() > last.getPrice()) {
+            setPriceFalling(true);
+        } else {
+            setPriceFalling(false);
+        }
     }
 }
