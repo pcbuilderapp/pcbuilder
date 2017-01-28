5aee893f41b9c729a5cff70439c2c1c36dee72c6
Stefan Ladage <sladage@gmail.com>
2017-01-29 00:38:16 +0100

    Added rate limiting.

---
+++ src/main/java/pcbuilder/WebMvcConfig.java
@@ -0,0 +1,20 @@
+package pcbuilder;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+@EnableScheduling
+public class WebMvcConfig extends WebMvcConfigurerAdapter {
+
+    @Autowired
+    RateLimitInterceptor rateLimitInterceptor;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(rateLimitInterceptor);
+    }
+}
